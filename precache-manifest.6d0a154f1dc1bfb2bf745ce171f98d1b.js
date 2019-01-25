c006bac5f514db375f236a949bfcd7c971cd93aa
Roman <antbobrikov@yandex.ru>
2019-01-25 22:40:26 +0300

    Updates

---
+++ precache-manifest.6d0a154f1dc1bfb2bf745ce171f98d1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "abeb34e14e8c27523bca",
+    "url": "/Redux_ToDo/static/js/runtime~main.abeb34e1.js"
+  },
+  {
+    "revision": "16ae160ccea111b49b06",
+    "url": "/Redux_ToDo/static/js/main.16ae160c.chunk.js"
+  },
+  {
+    "revision": "5163782db42110f50068",
+    "url": "/Redux_ToDo/static/js/1.5163782d.chunk.js"
+  },
+  {
+    "revision": "16ae160ccea111b49b06",
+    "url": "/Redux_ToDo/static/css/main.c7c6e4fb.chunk.css"
+  },
+  {
+    "revision": "6a91a7176f207aac96c7d40bbd5287ce",
+    "url": "/Redux_ToDo/index.html"
+  }
+];
